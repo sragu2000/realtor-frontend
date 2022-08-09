ec8288fb7cac837b0324a438086f43b032951193
gajen <gajen007@gmail.com>
2022-08-09 14:04:09 -0400

    second

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,39 +2,32 @@ import ListingGrid from '../Components/ListingGrid.js';
 import { useState, useEffect } from "react";
 
 function HomePage() {
-  const[listings]=useState([{"mlsnumber":"W5715268","price":"$739,000 ","address":"#G8 -284 MILL RD, Toronto, Ontario ","latitude":"43.6375647","longitude":"-79.5824898","bedrooms":"2 + 1","washrooms":"2","created_at":null,"updated_at":null}]);
-  /*  
-  useEffect(
-    () => {
+  const[listings]=useState([]);
+  const[grids]=useState([]);
+  
+  useEffect( ()=>{
       fetch('http://localhost:8000/api/allListings',{
         method:'GET',
         mode: 'cors',
         cache: 'no-cache'})
-      .then(res => { return res.json(); })
-      .then(data => {
-        var dataArray=Array.from(data['listings']); //important
-        dataArray.map(function(listing){
-          listings.push({
-            key:listing.id,
-            mlsnumber:listing.mlsnumber, 
-            price:listing.price, 
-            address:listing.address, 
-            latitude:listing.latitude, 
-            longitude:listing.longitude, 
-            bedrooms:listing.bedrooms, 
-            washrooms:listing.washrooms
+        .then(res => { return res.json(); })
+        .then(data => {
+          var dataArray=Array.from(data['listings']); //important
+          dataArray.map(function(item){
+            listings.push({
+              "mlsnumber":item['mlsnumber'],
+              "price":item['price'],
+              "address":item['address'],
+              "latitude":item['latitude'],
+              "longitude":item['longitude'],
+              "bedrooms":item['bedrooms'],
+              "washrooms":item['washrooms']
+            });
           });
-        });
-      }).catch(err => console.error(err));
-    },[]
-  );
-*/
-
-  return (
-    <div>
-    {
-      listings.map(function(item){
-        return <ListingGrid 
+        }).catch(err => console.error(err));
+        
+        listings.map((item)=>{
+          grids.push(<ListingGrid 
             key={item.mlsnumber}
             mlsnumber={item.mlsnumber} 
             price={item.price} 
@@ -43,11 +36,15 @@ function HomePage() {
             longitude={item.longitude} 
             bedrooms={item.bedrooms} 
             washrooms={item.washrooms}
-        />;
-      })
+            />);
+          });
+ },[listings]);
+    
+    return (
+      <div>
+      {grids}
+      </div>
+      )
     }
-    </div>
-  )
-  }
-  
-  export default HomePage;
+    
+    export default HomePage;
