9eb6e5bf4f80a2ef4539fdde1426bf768f1b50a2
sragu2000 <s.s.raguraj@gmail.com>
2022-08-21 22:41:12 +0530

    sample view

--- src/Pages/SingleListing.js
+++ src/Pages/SingleListing.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function SingleListing() {
   const { mlsNumber } = useParams();
+  const [jsonData, setJsonData]=useState({});
+  useEffect(()=>{
+    fetch("URL/"+mlsNumber,{method:'GET',mode: 'cors',cache: 'no-cache'})
+    .then(response => {
+        if (response.status == 200) {return response.json();}
+        else {console.log('Backend Error..!');console.log(response.text());}
+    })
+    .then(data => {
+        setJsonData(data);
+    })
+    .catch(() => {console.log("Network connection error");});
+  },[])
   //useState with varibles
     //address
     //price
@@ -25,7 +38,27 @@ function SingleListing() {
 
   return (
     <div className="container">
-      
+      <div className="card">
+        <div className="card-header" align="center">{jsonData.mlsnumber}</div>
+        <div className="card-body">
+          <div className="row mt-4">
+            <div className="col-md-3">Address</div>
+            <div className="col-md-9">{jsonData.address}</div>
+          </div>
+          <div className="row mt-4">
+            <div className="col-md-3">Bedrooms</div>
+            <div className="col-md-9">{jsonData.bedrooms}</div>
+          </div>
+          <div className="row mt-4">
+            <div className="col-md-3">Washrooms</div>
+            <div className="col-md-9">{jsonData.washrooms}</div>
+          </div>
+          <div className="row mt-4">
+            <div className="col-md-3">Price</div>
+            <div className="col-md-9">{jsonData.price}</div>
+          </div>
+        </div>
+      </div>
     </div>
     );
   }
