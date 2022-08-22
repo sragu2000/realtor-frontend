898dac7c88fda35e49e73b97872c778096751463
sragu2000 <s.s.raguraj@gmail.com>
2022-08-22 12:25:42 +0530

    listing page

--- src/Components/ListingGrid.js
+++ src/Components/ListingGrid.js
@@ -1,32 +1,105 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import propertyImage from "../Images/home.jpg"
 function ListingGrid(props) { //props is an object as a parameter
     return (
-        <div className="card border-dark" style={{margin:'1%'}}>
-            <div className="card-header" align="center">{props.mlsnumber}</div>
-            <div className="card-body">
-                <ul style={{'listStyle':'none'}}>
-                    <li>Address: {props.address}</li>
-                    <li>Price: {props.price}</li>
-                    <li>Bedrooms: {props.bedrooms}</li>
-                    <li>Washrooms: {props.washrooms}</li>
-                </ul>
-            </div>
-            <div className="card-footer" align="center">
-                <Link to={"singleListing/"+props.mlsnumber}>View Property</Link>
+        <div className="col">
+            <div className="card">
+                <div className="card-header bg-dark text-white text-center">{"MLS Number : " + props.mlsnumber}</div>
+                <img src={propertyImage} className="card-img-top" alt="property-image"></img>
+                <div className="card-body ">
+
+                    {/* Design 1 */}
+                    <div className="row border-bottom">
+                        <div className="col-md-2"><i className="fa-solid fa-map-location-dot"></i></div>
+                        <div className="col-md-10">
+                            <a href={"https://www.google.com/maps/search/?api=1&query=" + props.latitude + "%2C" + props.longitude} target="_blank">{props.address}</a>
+                        </div>
+                    </div>
+                    <div className="row border-bottom">
+                        <div className="col-md-2"><i className="fa-solid fa-bed"></i></div>
+                        <div className="col-md-10">{props.bedrooms+" Bed Rooms"}</div>
+                    </div>
+                    <div className="row border-bottom">
+                        <div className="col-md-2"><i className="fa-solid fa-bath"></i></div>
+                        <div className="col-md-10">{props.washrooms+" Wash Rooms"}</div>
+                    </div>
+                    <div className="row border-bottom">
+                        <div className="col-md-2">
+                            <i class="fa-solid fa-dollar-sign"></i>
+                        </div>
+                        <div className="col-md-10">{props.price}</div>
+                    </div>
+
+
+                    {/* Design 2 */}
+                    {/* <table class="table table-hover table-bordered">
+                        <tbody>
+                            <tr className="table-success">
+                                <td scope="row"><i class="fa-solid fa-map-location-dot"></i> &nbsp; Address</td>
+                                <td>
+                                    <a href={"https://www.google.com/maps/search/?api=1&query=" + props.latitude + "%2C" + props.longitude} target="_blank">{props.address}</a>
+                                </td>
+                            </tr>
+                            <tr className="table-info">
+                                <td scope="row"><i class="fa-solid fa-bed"></i> &nbsp; Bed Rooms</td>
+                                <td>{props.bedrooms + " Rooms"}</td>
+                            </tr>
+                            <tr className="table-success">
+                                <td scope="row"><i class="fa-solid fa-bath"> &nbsp; </i>Wash Rooms</td>
+                                <td>{props.washrooms + " Rooms"}</td>
+                            </tr>
+                            <tr className="table-info">
+                                <td scope="row"><i class="fa-solid fa-dollar-sign"></i>&nbsp;Price</td>
+                                <td>{props.price}</td>
+                            </tr>
+                        </tbody>
+                    </table> */}
+
+                    {/* Design 3 */}
+                    {/* <div className="row">
+                        <div className="col-md-5">
+                            <i class="fa-solid fa-map-location-dot"></i> &nbsp;Address&nbsp;:
+                        </div>
+                        <div className="col-md-7">
+                            <a href={"https://www.google.com/maps/search/?api=1&query=" + props.latitude + "%2C" + props.longitude} target="_blank">{props.address}</a>
+                        </div>
+                    </div>
+                    
+                    
+                    <p></p>
+                
+                    <i class="fa-solid fa-bed"></i> &nbsp; Bed Rooms&nbsp;:&nbsp;
+                    {props.bedrooms + " Rooms"}
+                    <p></p>
+                
+                    <i class="fa-solid fa-bath"> &nbsp; </i>Wash Rooms&nbsp;:&nbsp;
+                    {props.washrooms + " Rooms"}
+                    <p></p>
+                
+                    <i class="fa-solid fa-dollar-sign"></i>&nbsp;Price&nbsp;:&nbsp;
+                    {props.price} */}
+                    {/* Design 3 end*/}
+
+                </div>
+                <div className="card-footer">
+                    <div className="row">
+                        <div className="col">
+                            <a href="#" className="btn btn-outline-primary form-control">
+                                <i className="fa-solid fa-comments"></i>&nbsp;
+                                Enquiry
+                            </a>
+                        </div>
+                        <div className="col">
+                            <a href="#" className="btn btn-outline-success form-control">
+                                <i className="fa-solid fa-magnifying-glass-location"></i>&nbsp;
+                                Show
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
 }
 export default ListingGrid; //necessary to be implemented
-
-//props.studentName object's field
-//props.studentEmail object's field
-/*
-{
-props.id
-props.latitude
-props.longitude
-}
-
-*/
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,37 +2,42 @@ import ListingGrid from '../Components/ListingGrid.js';
 import { useState, useEffect } from "react";
 
 function HomePage() {
-  const[listings,setListings]=useState([]);
+  const [listings, setListings] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
 
-      fetch('http://localhost:8000/api/allListings',{
-      method:'GET',
+    fetch('http://localhost:8000/api/allListings', {
+      method: 'GET',
       mode: 'cors',
-      cache: 'no-cache'})
+      cache: 'no-cache'
+    })
       .then(res => { return res.json(); })
       .then(data => {
         setListings(data['listings']);
-        }).catch(err => console.error(err));
+      }).catch(err => console.error(err));
 
-    },[]);
+  }, []);
 
-    return (<>
-    {
-      listings.map((item)=>{
-        return (<ListingGrid 
-          key={item.mlsnumber}
-          mlsnumber={item.mlsnumber} 
-          price={item.price}
-          address={item.address} 
-          latitude={item.latitude} 
-          longitude={item.longitude} 
-          bedrooms={item.bedrooms} 
-          washrooms={item.washrooms}
-          />);
-        })
-      }
-      </>);
-  }
-  
-  export default HomePage;
+  return (
+    <div className="container mt-5">
+      <div class="row row-cols-1 row-cols-md-4 g-4">
+        {
+          listings.map((item) => {
+            return (<ListingGrid
+              key={item.mlsnumber}
+              mlsnumber={item.mlsnumber}
+              price={item.price}
+              address={item.address}
+              latitude={item.latitude}
+              longitude={item.longitude}
+              bedrooms={item.bedrooms}
+              washrooms={item.washrooms}
+            />);
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
